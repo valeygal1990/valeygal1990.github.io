5cf009b8ab7a45373445b0f8277371a8139715f3
valeygal1990 <valentinesakwa@Valentines-MacBook-Air.local>
2016-05-13 13:46:12 -0400

    Js pseudo code

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
 
+	//I think I will save all the forms in an array..
+
 //User selects the date from the dropdown form
 //User clicks on the next button
 //Date is saved in a variable and will later be inerted into the final page form
 //date form is hidden
 //child's name form is shown
 //user fills in child's name
+//
 
 $('form').submit(saveValue);
 
