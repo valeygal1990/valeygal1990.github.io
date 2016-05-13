f8bd8cb44ad17f31806114cac08671144958f8f8
valeygal1990 <valentinesakwa@Valentines-MacBook-Air.local>
2016-05-13 10:47:32 -0400

    jQuery functions!

--- js/index.js
+++ js/index.js
@@ -6,14 +6,25 @@ $(document).ready(function() {
 //date form is hidden
 //child's name form is shown
 //user fills in child's name
+
+$('form').submit(saveValue);
+
+function saveValue() {
+	var childsName=$('#childsNameForm').val();
+
+	if childsName ==''{
+		event.preventDefault();
+		$('#childsNameForm').hide();
+    	$('#caregiverform').show();
+    	console.log ('#childsNameForm');
+    	console.log ('#childsNameForm');
+	}
+}
+
 //User clicks on the next button
 //value of the input field is saved in a variable to be inserted later into the final form
 
 
-	$('.btn-group').click(doSomething);
-
-function doSomething() {
-	// make something happen here
-}
+	
 
 });
