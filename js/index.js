7e07cc3f0022b9ef78ae412f7c91d318dbcd4c37
valeygal1990 <valentinesakwa@Valentines-MacBook-Air.local>
2016-05-13 10:27:04 -0400

    js pseudo code

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+//User selects the date from the dropdown form
+//User clicks on the next button
+//Date is saved in a variable and will later be inerted into the final page form
+//date form is hidden
+//child's name form is shown
+//user fills in child's name
+//User clicks on the next button
+//value of the input field is saved in a variable to be inserted later into the final form
+
+
 	$('.btn-group').click(doSomething);
 
 function doSomething() {
